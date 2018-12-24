3c0ddf214b947c3fb73777b9e685d12a4691a49c
wooken <kenny@kenwoo.me>
2018-12-23 16:29:49 -0800

    Rewrite with more useful info

--- docs/work.js
+++ docs/work.js
@@ -1,33 +1,12 @@
-function days_since() {
-    var date0 = new Date(2018, 4, 30); // May 30, 2018 is day 1
-    var date1 = new Date();
-    var days_since_date0 = Math.ceil((date1 - date0) / 8.64e7);
-    return days_since_date0;
-}
+var weeks_pushed_back = 8;
+var total_days = 365 + weeks_pushed_back * 7;
+var start_day = new Date(2018, 4, 30); // May 30, 2018 is day 1
+var last_day = new Date(start_day.getTime());
+last_day.setDate(last_day.getDate() + total_days);
 
-function weeks_since() {
-    days = days_since();
-    weeks = days / 7;
-    mod = days % 7;
-    if (mod == 0) {
-        return weeks;
-    } else {
-        return Math.floor(weeks) + 1;
-    }
-}
+var total_months = Math.round(total_days / 30, 2);
 
-function days_left() {
-    days = days_since();
-    return 365 - days;
-}
-
-function weeks_left() {
-    weeks = weeks_since();
-    return 52 - weeks;
-}
-
-function current_month() {
-    weeks = weeks_since();
-    months = weeks / 4;
-    return months;
-}
+var ms_per_month = 30*24*60*60*1000; // days*hours*minutes*seconds*milliseconds
+var today = new Date();
+var months_elapsed = Math.round(((last_day.getTime() - today.getTime()) / ms_per_month), 2);
+var months_left = total_months - months_elapsed;
